bcb647b37438327ecc8e7b4ad1cb4580f0f2a1f4
rounak <rounakkole@gmail.com>
2022-07-16 22:25:48 +0530

    initial commit 2

---
+++ loading.py
@@ -0,0 +1,21 @@
+def loader(progress, total):
+    #from os import system
+
+    percentageR = total / 100
+    temp = progress / percentageR
+
+    if (temp <= 100):
+        #system('clear')
+        print("|", end="")
+        while (temp > 4):
+            print("█", end="")
+            temp = temp - 5
+
+        remainder = 100 - ((progress / percentageR) - temp)
+        while (remainder > 4):
+            print("-", end="")
+            remainder = remainder - 5
+
+        print("|", end="  ")
+        print(int(progress / total * 100), end="\n \n")
+        
